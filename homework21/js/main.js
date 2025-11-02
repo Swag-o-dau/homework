f44bfd3ed7502afb64c010ba6d92fa8f89a9d9c1
Swag-o-dau <166879047+Swag-o-dau@users.noreply.github.com>
2025-11-02 13:11:46 +0300

    Update main.js

--- homework21/js/main.js
+++ homework21/js/main.js
@@ -115,7 +115,7 @@
 
     new Swiper('.gallery__slider', {
 
-        spaceBetween: 15,
+        spaceBetween: 8,
         slidesPerView: 1.5,
 
         pagination: {
@@ -130,6 +130,7 @@
 
         breakpoints: {
             451: {
+                spaceBetween: 15,
                 slidesPerView: 2,
             },
             601: {
@@ -173,5 +174,6 @@
         }
 
     });
+
 
 })()
